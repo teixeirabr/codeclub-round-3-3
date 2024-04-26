54b5bb62736faf67a1c6043f203fec49646cbd8e
andrelaprov@gmail.com <andrelaprov@gmail.com>
2024-04-26 00:20:09 -0300

    adding conditionals and operators

--- src/index.js
+++ src/index.js
@@ -10,12 +10,25 @@
 
  var src_default = {
 	async fetch(request, env, ctx) {
-		console.log("Logging: " + request.url)
-	  return new Response("Hello World!");
+	  if(request.url == "https://www.cloudflareworkers.com/test") {
+			 return new Response('Hello worker!', {
+				 headers: {
+					 'content-type': 'text/plain',
+				 },
+			 });
+		 }
+		 else{
+			 return new Response('Error Worker! Wrong URL', {
+				 headers: {
+					 'content-type': 'text/plain',
+				 },
+			 });
+		 }
 	}
   };
   export {
 	src_default as default
   };
+  
   
 
