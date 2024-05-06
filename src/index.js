fc4ba1650aeb7a9e2d9ebb910c1a0b881a2b405a
teixeirabr <59811970+teixeirabr@users.noreply.github.com>
2024-05-06 01:07:52 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,27 +8,41 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
- var src_default = {
-	async fetch(request, env, ctx) {
-	  if(request.method == "POST") {
-			 return new Response('You sent a POST', {
-				 headers: {
-					 'content-type': 'application/json',
-				 },
-			 });
-		 }
-		 else{
-			 return new Response('Error!! Please use POST method', {
-				 headers: {
-					 'content-type': 'application/json',
-				 },
-			 });
-		 }
-	}
-  };
-  export {
-	src_default as default
-  };
-  
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+const responses = [
+  "May the Force be with you!",
+  "The dark side clouds everything.",
+  "Do or do not. There is no try.",
+  "Fear is the path to the dark side. Fear leads to anger, anger leads to hate, hate leads to suffering."
+];
+
+async function fetchRandomCharacter() {
+  const randomIndex = Math.floor(Math.random() * 82) + 1;
+  const characterData = await fetch(`https://swapi.dev/api/people/${randomIndex}`);
+  const characterJson = await characterData.json();
+  return characterJson.name;
+}
+
+async function handleRequest(request) {
+  const characterName = await fetchRandomCharacter();
+
+  const randomIndex = Math.floor(Math.random() * responses.length);
+  const randomResponse = responses[randomIndex];
+
+  // Construct the final response with random response and character name
+  const finalResponse = `Star Wars' Quote Of The Day: ${randomResponse}\n\nStar Wars' Character Of The Day: ${characterName}`;
+
+  // Set response headers
+  const headers = new Headers();
+  headers.set('Content-Type', 'text/plain');
+
+  return new Response(finalResponse, {
+    headers
+  });
+}
+
   
 
